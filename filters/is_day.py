a54b248fcbdcd64ec524de1e6281564c6eedb6ae
Михаил 1 <m315498@gmail.com>
2020-12-27 14:58:27 +0300

    added handler for button "now"

--- filters/is_day.py
+++ filters/is_day.py
@@ -1,12 +1,11 @@
 from aiogram import types
 from aiogram.dispatcher.filters import BoundFilter
 
-from data.convert import to_eng, to_rus
+from data.convert import to_rus
 
 
 class DayFilter(BoundFilter):
     key = 'is_day'
 
     async def check(self, message: types.Message):
-        print(message.text.lower() in to_rus)
         return message.text.lower() in to_rus
--- handlers/users/message_handlers.py
+++ handlers/users/message_handlers.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from aiogram import types
 from aiogram.dispatcher.filters import Text
 from aiogram.utils.markdown import hbold, hitalic
@@ -9,13 +11,13 @@ from keyboards.default import menu
 from keyboards.inline.inline_buttons import check_week, kb_more, get_group_buttons, get_rating_kb
 
 from loader import dp
+from models.lessons import Lesson
 from models.week import Week, ThisNextWeek
 
 from utils.db_api.commands.commands_teacher import select_teacher_by_name, get_rating
 from utils.db_api.commands.coomands_group import select_group, select_group_id
-from utils.db_api.schemas.group import Groups
 from utils.db_api.schemas.user import User
-from utils.db_api.commands.commands_timetable import get_some_day, check_existence
+from utils.db_api.commands.commands_timetable import get_some_day, check_existence, get_day_raw
 
 
 @dp.message_handler(Text(equals=['сегодня', 'завтра'], ignore_case=True))
@@ -52,8 +54,52 @@ async def get_day_week(message: types.Message, user: User):
 
 @dp.message_handler(Text('сейчас', ignore_case=True))
 async def get_now(message: types.Message, user: User):
-    pass
-    # TODO
+    week = ThisNextWeek.this_week
+    day = Week.today()
+    group = user.group_id
+    subgroup = user.subgroup
+    now = float(datetime.strftime(datetime.now(), "%H.%M"))
+    schedule = await get_day_raw(day, group, subgroup, week)
+    if not schedule:
+        return await message.answer("Сегодня нет пар!", reply_markup=menu)
+    if now > Lesson(schedule[-1][0]).to_float_tuple()[1]:
+        return await message.answer("Сегодня больше не будет пар!", reply_markup=menu)
+    now_lesson = ''
+    next_lesson = ''
+    if now < Lesson(schedule[0][0]).to_float_tuple()[0]:
+        lesson = schedule[0][1]
+        time = Lesson(schedule[0][0])
+        return await message.answer(
+            hbold('Следующим:\n\n') +
+            f"{time.to_emoji()} {lesson} {time.to_str()}",
+            reply_markup=menu
+        )
+    for num, lesson in enumerate(schedule):
+        time = Lesson(lesson[0])
+        time_tuple = time.to_float_tuple()
+        if now > time_tuple[1]:
+            continue
+        begin = time_tuple[0]
+        end = time_tuple[1]
+        if begin <= now <= end:
+            now_lesson = f"{time.to_emoji()} {lesson[1]} {time.to_str()}"
+            num += 1
+        elif now < begin:
+            now_lesson = "Премена"
+        if num < len(schedule) and now_lesson:
+            time = Lesson(schedule[num][0])
+            next_lesson = f"{time.to_emoji()} {schedule[num][1]} {time.to_str()}"
+            break
+        elif now_lesson:
+            next_lesson = "Сегодня больше не будет пар!"
+            break
+    txt = [
+        hbold("Сейчас:"),
+        now_lesson,
+        hbold("Следующим:"),
+        next_lesson
+    ]
+    await message.answer('\n\n'.join(txt), reply_markup=menu)
 
 
 @dp.message_handler(Text(equals=['еще', 'ещё'], ignore_case=True))
