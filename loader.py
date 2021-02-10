6bf4886b50396613e229bc7e121f488a5089f98e
rdfsx <m315498@gmail.com>
2021-02-10 11:54:40 +0300

    eifeirhireug

--- loader.py
+++ loader.py
@@ -1,4 +1,5 @@
 from aiogram import Bot, Dispatcher, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
@@ -9,7 +10,8 @@ import config
 from utils.db_api.db_gino import db
 
 bot = Bot(token=config.BOT_TOKEN, parse_mode=types.ParseMode.HTML)
-storage = RedisStorage2(host=config.REDIS_HOST)
+# storage = RedisStorage2(host=config.REDIS_HOST)
+storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
 scheduler = AsyncIOScheduler()
 
