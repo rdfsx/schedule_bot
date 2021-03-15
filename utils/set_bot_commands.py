69f56401d151d298704d6b0384a559e6f5378a1b
rdfsx <m315498@gmail.com>
2021-03-15 10:22:41 +0300

    fix

--- utils/set_bot_commands.py
+++ utils/set_bot_commands.py
@@ -5,7 +5,7 @@ async def set_default_commands(dp):
     await dp.bot.set_my_commands([
         types.BotCommand('b', 'Если исчезли кнопки'),
         types.BotCommand('reset', 'Сброс настроек'),
-        types.BotCommand('prepods', 'Рейтинг преподавателей'),
+        types.BotCommand('prepods', 'Рейтинг и расписание преподавателей'),
         types.BotCommand('search', 'Расписание чужой группы'),
         types.BotCommand('donuts', 'Поддержать разработчика'),
         types.BotCommand('calls', 'Стикер с расписанием звонков')
